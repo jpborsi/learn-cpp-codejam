102f8823a3da661a2c0eaaff4ff905408406bb3c
Jbroski <jpb114@case.edu>
2016-07-01 07:37:34 -0400

    Version 0.2 2013 Q-A

--- 2013/Q/Q-A.cpp
+++ 2013/Q/Q-A.cpp
@@ -20,6 +20,31 @@ When you run your code on the sample input, it should create the sample output e
 using namespace std;
 
 int getGameState(char* board[4]){
+	int Xwin = (1<<11)-1, Owin = (1<<11)-1, fill = 0;
+	int Xt = 0, Ot = 0;
+	for(int i=0;i<4;i++){
+		int m = 1;
+		for(int j=0;j<4;j++){
+			//cout << i << "," << j << endl;
+			Xt += m*1*(board[i][j] == 'X' || board[i][j] == 'T');
+			Xt += m*2*(board[j][i] == 'X' || board[i][j] == 'T');
+			Ot += m*1*(board[i][j] == 'O' || board[i][j] == 'T');
+			Ot += m*2*(board[i][j] == 'O' || board[i][j] == 'T');
+			fill |= (board[i][j]=='.');
+			m *= 2;
+		}
+		Xt += m*1*(board[i][i] == 'X' || board[i][i] == 'T');
+		Xt += m*2*(board[3-i][3-i] == 'X' || board[3-i][3-i] == 'T');
+		Ot += m*1*(board[i][i] == 'O' || board[i][i] == 'T');
+		Ot += m*2*(board[3-i][3-i] == 'O' || board[3-i][3-i] == 'T');
+		Xwin &= Xt;
+		Owin &= Ot;
+		cout << Xt << "," << Xwin << endl;
+	}
+
+	if(Xwin>0){return 2;}
+	if(Owin>0){return 3;}
+	if(!fill){return 1;}
 	return 0;
 }
 
