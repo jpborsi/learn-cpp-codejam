28628bf648ba759480025f20955e78a08a193c78
Jbroski <jpb114@case.edu>
2016-07-01 20:09:19 -0400

    Version 0.0 2011 Q-A

---
+++ 2011/Q/Q-A.cpp
@@ -0,0 +1,38 @@
+// 2011 Qualification Round, Problem 1
+// Bot Trust
+// https://code.google.com/codejam/contest/975485
+
+/*
+Input: The first line of the input gives the number of test cases, T. T test cases follow.
+
+Each test case consists of a single line beginning with a positive integer N, representing the number of buttons that need to be pressed.
+This is followed by N terms of the form "Ri Pi" where Ri is a robot color (always 'O' or 'B'), and Pi is a button position.
+*/
+
+/*
+Output: For each test case, output one line containing "Case #x: y", where x is the case number (starting from 1)
+and y is the minimum number of seconds required for the robots to push the given buttons, in order.
+*/
+#include <iostream>
+
+using namespace std;
+
+int getMinSeconds(char*, int*){
+	return 0;
+}
+
+int main(){
+	int T, N;
+	char* R;
+	int* P;
+	cin >> T;
+	for(int i = 1; i<= T; i++){
+		cin >> N;
+		R = new char[N];
+		P = new int[N];
+		for(int j = 0; j<N; j++){
+			cin >> R[j] >> P[j];
+		}
+		cout << "Case #" << i << ": " << getMinSeconds(R,P) << endl;
+	}
+}
