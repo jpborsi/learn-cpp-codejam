b49a7c8d24aaf9372f54044fb33ebd11fb416874
john.borsi <jborsi@us.ibm.com>
2017-04-27 09:51:07 -0400

    2013 1A-A work

--- 2013/1A/1A-A.cpp
+++ 2013/1A/1A-A.cpp
@@ -11,35 +11,19 @@ Output: For each test case, output one line containing "Case #x: y", where x is 
 */
 
 #include <iostream>
-#include <gmp.h>
 
 using namespace std;
 
-char* getRings(int R, int P){
-	char* result [20];
-	mpz_t lower_limit, upper_limit;
-	mpz_init(lower_limit);
-	mpz_init_set_si(upper_limit, P);
-	mpz_t span, halfSpan, midpoint;
-	mpz_t ONE, TWO;
-	mpz_init(span);
-	mpz_init(halfSpan);
-	mpz_init(midpoint);
-	mpz_init_set_si(TWO, 2);
-	mpz_init_set_si(ONE, 1);
-	while(mpz_comp(lower_limit,upper_limit)<0){
-		mpz_sub(span,upper_limit,lower_limit);
-		mpz_cdiv_q(halfSpan,span,TWO);
-		mpz_add(midpoint,lower_limit,halfSpan);
-		if(mpz_comp(midpoint,lower_limit)){
-			mpz_add(midpoint, lower_limit, ONE);
-		}
-		if(canPaint(midpoint,R,P)){
-			
-		}
+bool canPaint(long long int startingRings, long long int amountOfPaint, long long int possible){
+	return amountOfPaint > 2*possible*possible + 4*startingRings*possible + 4*possible;
+}
+
+long long int getRings(int R, int P){
+	int checkRings = 2;
+	while(canPaint(R,P,checkRings)){
+		checkRings <<= 1;
 	}
-	mpz_get_str(result,10,lower_limit);
-	return result;
+	return checkRings;
 }
 
 int main(){
