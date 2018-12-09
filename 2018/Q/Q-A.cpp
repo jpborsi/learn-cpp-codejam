16a0705832a891c2d67d4435fe022fb79b68cdb2
john.borsi <jpb114@case.edu>
2018-12-09 15:04:25 -0500

    Fixed 2018 Q-A

--- 2018/Q/Q-A.cpp
+++ 2018/Q/Q-A.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <math.h>
+#include <assert.h>
 using namespace std;
 
 int getMinHacks(long D, string P) {
@@ -23,18 +24,22 @@ int getMinHacks(long D, string P) {
 	else if(totalDamage <= D){ return 0; }
 
 	int minHacks = 0;
-	for(int i = 0; i < 30; i++){
+	for(int i = 0; i < numShots*numCharges; i++){
 		int maxJ = 0;
-		for(int j = 0; j < P.size(); j++){
+		for(int j = 0; j <= numCharges; j++){
 			if(shotStrength[j]>0){
 				maxJ = j;
 			}
 		}
+		assert(maxJ > 0);
 		shotStrength[maxJ]--;
-		totalDamage -= pow(2, maxJ-1);
+		shotStrength[maxJ-1]++;
+		int strengthDiff = 1 << (maxJ-1);
+		totalDamage -= strengthDiff;
 		minHacks++;
 		if(totalDamage <= D){ return minHacks; }
 	}
+	assert(false);
 	return -1;
 }
 
