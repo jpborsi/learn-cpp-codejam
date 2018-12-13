95a2396b2adac4c3cbfcc64d7fa6610be43ddf63
John Borsi <jborsi@us.ibm.com>
2018-12-13 11:27:30 -0500

    Fixed 2017 Q-C

--- 2017/Q/Q-C.cpp
+++ 2017/Q/Q-C.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
 #include <cmath>
 #include <stdio.h>
+#include <map>
 using namespace std;
 
-long long int getSize(long long int N, long long int K){
-	int level = floor(log(K)/log(2));
-	long long int numberOfPeopleInLevel = pow(2,level);
-	long long int numberOfSeatsInLevel = N - numberOfPeopleInLevel + 1;
-	long long int goodSeatAmount = ceil(1.0*numberOfSeatsInLevel/numberOfPeopleInLevel);
-	long long int badSeatAmount = floor(1.0*numberOfSeatsInLevel/numberOfPeopleInLevel);
-	long long int numberOfGoodSeats = numberOfSeatsInLevel - badSeatAmount*numberOfPeopleInLevel;
-	if(K - pow(2,level) + 1 <= numberOfGoodSeats){
-		return goodSeatAmount;
-	}else{
-		return badSeatAmount;
+typedef long long int lli;
+
+void solve(lli stalls, lli people){
+	auto reverseCmp = [](const lli &a, const lli &b) -> bool {return a>b;};
+	map<lli, lli, decltype(reverseCmp)> sizes(reverseCmp);
+	sizes[stalls] =  1;
+	lli currentCount = 0;
+	lli x0, x1;
+	while(currentCount < people){
+		auto max = sizes.begin();
+		lli blockSize = max->first;
+		lli count = max->second;
+		sizes.erase(blockSize);
+		x0 = (blockSize-1)/2 + (blockSize-1)%2;
+		x1 = (blockSize-1)/2;
+		if(sizes.find(x0) == sizes.end()){
+			sizes[x0] = 0;
+		}
+		sizes[x0] += count;
+		if(sizes.find(x1) == sizes.end()){
+			sizes[x1] = 0;
+		}
+		sizes[x1] += count;
+		currentCount += count;
 	}
+	cout << x0 << " " << x1 << endl;
 }
 
 int main()
@@ -24,17 +39,8 @@ int main()
 	cin >> T;
 	for (int i = 1; i <= T; i++) {
 		cin >> N >> K;
-		long long int effectiveSize = getSize(N,K);
-		//cout << effectiveSize << endl;
 		cout << "Case #" << i << ": ";
-		if(effectiveSize <= 1){
-			cout << 0 << " " << 0 << endl;
-		}else{
-			long long int upper = ceil(1.0*(effectiveSize-1)/2);
-			long long int lower = floor(1.0*(effectiveSize-1)/2);
-			printf ("%lld %lld\n", upper, lower );
-			//cout << ceil(1.0*(effectiveSize-1)/2) << " " << floor(1.0*(effectiveSize-1)/2) << endl;
-		}
+		solve(N, K);
 	}
 	return 0;
 }
